27861dd7ee256f27b5ef91b52680b3f730c89672
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-08 13:12:56 -0600

    feat: se realizo la conexion con mongo db

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from 'express'; //Trae la función principal de Express
+import {notaRouter} from "./routes/nota.routes.js";
+import {manejarError, rutaNoEncontrada} from "./middlewares/error.middleware.js"
+
 const app = express(); // crea nuestra aplicación web
 
 app.use(express.json());//  convierte el Json recibido en req.body
@@ -6,6 +9,9 @@ app.use(express.json());//  convierte el Json recibido en req.body
 app.get('/health', (req, res) => { //atiende GET/ health
   res.status(200).json({ status: 'ok' }) //Devuelve estado HTTP 200 y un objeto JSON
 });
+
+app.use("/api/v1/notas",notaRouter);
+app.use(rutaNoEncontrada)
 
 export default app; // Permite usar app desde server.js
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
+import mongoose from 'mongoose';
 import app from './app.js'; // importa la aplicación preparada
 const PORT =3000; //Define el puerto local 
+const MONGODB_URI="mongodb+srv://lopezjennifer8642_db_user:wCPokEn5bEsKpJO5@campustec.piaiqxo.mongodb.net/"
 
-app.listen(PORT, () => { 
+try{
+    await mongoose.connect(MONGODB_URI);
+    app.listen(PORT, () => { 
     console.log(`API lista en http://localhost:${PORT}`); //Muestá la dirección
- 
-}); // Cierra el arranque
+    }); // Cierra el arranque
+}catch(error){
+    console.log("No fue posible conectar con MongoDB", error.message);
+    process.exit(1);
+}
